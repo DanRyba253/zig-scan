51daaa6f8b3e7c71ce25eec9cd198511d667d89a
Dani Rybe <danilrybakov249@gmail.com>
2025-02-16 15:29:18 +0700

    eliminate dependency on the find command for test generation

--- build.zig
+++ build.zig
@@ -13,41 +13,46 @@ pub fn build(b: *std.Build) void {
 
     // try to generate test step
     generate_test_step: {
+        var src = std.fs.cwd().openDir(src_dir, .{
+            .iterate = true,
+        }) catch break :generate_test_step;
+        defer src.close();
+
         var gpa = std.heap.GeneralPurposeAllocator(.{}){};
         defer _ = gpa.deinit();
         const allocator = gpa.allocator();
-        const argv = [_][]const u8{ "find", src_dir, "-name", "*.zig" };
 
-        const r = std.process.Child.run(.{
-            .allocator = allocator,
-            .argv = &argv,
-        }) catch break :generate_test_step;
-
-        defer {
-            allocator.free(r.stdout);
-            allocator.free(r.stderr);
-        }
+        var walker = src.walk(allocator) catch break :generate_test_step;
+        defer walker.deinit();
 
         const run_all_tests = b.step("test", "Run all tests");
 
-        var split_iter = std.mem.splitScalar(u8, r.stdout, '\n');
+        while (walker.next() catch break :generate_test_step) |entry| {
+            if (entry.kind != .file) continue;
 
-        while (split_iter.next()) |line| {
-            if (line.len == 0) continue;
+            if (std.mem.endsWith(u8, entry.basename, ".zig")) {
+                const path_len = src_dir.len + 1 + entry.path.len;
+                var path = allocator.alloc(u8, path_len) catch break :generate_test_step;
+                defer allocator.free(path);
 
-            const file_tests = b.addTest(.{
-                .root_source_file = b.path(line),
-                .target = target,
-                .optimize = optimize,
-            });
+                std.mem.copyForwards(u8, path, src_dir);
+                path[src_dir.len] = std.fs.path.sep;
+                std.mem.copyForwards(u8, path[src_dir.len + 1 ..], entry.path);
 
-            if (b.args) |args| {
-                file_tests.filters = args;
+                const file_tests = b.addTest(.{
+                    .root_source_file = b.path(path),
+                    .target = target,
+                    .optimize = optimize,
+                });
+
+                if (b.args) |args| {
+                    file_tests.filters = args;
+                }
+
+                const run_file_tests = b.addRunArtifact(file_tests);
+                run_file_tests.has_side_effects = true;
+                run_all_tests.dependOn(&run_file_tests.step);
             }
-
-            const run_file_tests = b.addRunArtifact(file_tests);
-            run_file_tests.has_side_effects = true;
-            run_all_tests.dependOn(&run_file_tests.step);
         }
     }
 }
